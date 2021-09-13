b937485575f452e76100cdce80579bfe9f5ad8f9
shivamkumraa9 <shivamkumraa9@gmail.com>
2021-09-13 11:35:33 +0530

    Static files secured

--- shivamkumraa/urls.py
+++ shivamkumraa/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 
 from simple.sitemaps import BlogSitemap, StaticSitemap, BlogListSitemap
@@ -27,5 +26,8 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
